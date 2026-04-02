c2e4bd9efd2936e39b29540d24cbddbaf8ff7e11
meetindianic <mitesh.parmar@indianic.com>
2026-04-02 12:30:08 +0530

    changes

--- config/db.js
+++ config/db.js
@@ -3,7 +3,8 @@ require('dotenv').config();
 
 const connectDB = async () => {
     try {
-        const conn = await mongoose.connect(process.env.MONGO_URI || 'mongodb://localhost:27017/gatepass_db');
+        // const conn = await mongoose.connect(process.env.MONGO_URI || 'mongodb://localhost:27017/gatepass_db');
+        const conn = await mongoose.connect(process.env.MONGO_URI || 'mongodb+srv://meetmask_db_user:Lo9JNQapOMekn1J8@cluster90883.1rmjz.mongodb.net/');
         console.log(`MongoDB Connected: ${conn.connection.host}`);
     } catch (error) {
         console.error(`Error: ${error.message}`);
